e72d20d502ee042fbc5f087770b5caea38f9a83b
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-31 13:13:50 +0000

    connect backend with the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const baseUrl = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:4000'
+  : ''
 
 function App() {
+  const [ data, setData] = useState(null)
+
+  useEffect(() => {
+    axios.get(baseUrl + '/api/friends')
+      .then(res => {
+        setData(res.data);
+      })
+  }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {
+            data && data[0].name
+          }
+          Hello!
         </p>
         <a
           className="App-link"
